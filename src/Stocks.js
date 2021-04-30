723352b9964866deee28dd7d246eb3a8f8a30aa5
April Day <april.day@gmail.com>
2021-04-30 14:40:45 +0100

    Fourth commit

--- src/Stocks.js
+++ src/Stocks.js
@@ -21,6 +21,22 @@ export const Stocks= ()=> {
                     );
                 })}
             </div>
+
+            <div className="radioButtons">        
+            {stockData.map((data, key) => {
+                return (
+                    <div key={key}>
+                        <input type="radio" value={data.company} name="moneyStuff"/>{data.company}<br/>
+                        <input type="radio" value={data.ticker} name="moneyStuff"/>{data.ticker} <br/>
+                        <input type="radio" value={data.stockPrice} name="moneyStuff"/>{data.stockPrice} <br/>
+                        <input type="radio" value={data.timeElapsed} name="moneyStuff"/>{data.timeElapsed} <br/>
+                        <br/>
+                        <br/>
+                    </div>
+                    );
+                })}
+            </div>
         </>
-    );
+
+    ); {/*close return */}
 };
