1a6a11e1f42537a9a9bec25a7b4a5d1ce822f9df
April Day <april.day@gmail.com>
2021-05-10 09:50:36 +0100

    Fifteenth commit

--- src/OneProblem.js
+++ src/OneProblem.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.css";
 import { stockData } from "./data.js";
 
@@ -9,16 +10,22 @@ export const OneProblem = () => {
    // let thisCompany = stockData[randomElement];
    // console.log("this is the thisCompany (array position): ", thisCompany);
 
+   const thisCompany="April"; 
+
     function random(){
-        const randomElement = Math.floor(Math.random()*stockData.length);
-        console.log("this is the randomElement:", randomElement);
-        const thisCompany = stockData[randomElement];
+        React.useEffect(()=>{
+            window.addEventListener('keydown', (event) => {
+                const randomElement = Math.floor(Math.random()*stockData.length);
+                console.log("this is the randomElement:", randomElement);
+                thisCompany = stockData[randomElement];
+            });
+        }, []);
     }
 
         return ( 
             <>
                 <div>
-                     <p name="moneyStuff" onClick={this.random}/>{thisCompany.company}<br/> 
+                     <p name="moneyStuff" onClick={random}/>{thisCompany.company}<br/> 
                      <input type="radio" value={thisCompany.ticker} name="moneyStuff"/>{thisCompany.ticker} <br/>
                      <input type="radio" value={thisCompany.stockPrice} name="moneyStuff"/>{thisCompany.stockPrice} <br/>
                      <input type="radio" value={thisCompany.timeElapsed} name="moneyStuff"/>{thisCompany.timeElapsed} <br/>
