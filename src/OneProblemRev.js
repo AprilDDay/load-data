552dd4d49f9a2d2cab8a65c541d24533fe6bd4a8
April Day <april.day@gmail.com>
2021-05-11 09:19:23 +0100

    Eighteenth commit

--- src/OneProblemRev.js
+++ src/OneProblemRev.js
@@ -4,16 +4,24 @@ import { stockData } from "./data.js";
 
 class OneProblemRev extends React.Component {    
 
+//    let thisCompany = "April";
+
+
     random(){
-        React.useEffect(()=>{
+        //React.useEffect(()=>{
             window.addEventListener('keydown', (event) => {
     
                 const randomElement = Math.floor(Math.random()*stockData.length);
                 console.log("this is the randomElement:", randomElement);
                 const thisCompany = stockData[randomElement];
-
+                <>
+                    <p name="moneyStuff" />{thisCompany.company}<br/> 
+                        <input type="radio" value={thisCompany.ticker} name="moneyStuff" onClick={this.random}/>{thisCompany.ticker} <br/>
+                        <input type="radio" value={thisCompany.stockPrice} name="moneyStuff" onClick={this.random}/>{thisCompany.stockPrice} <br/>
+                        <input type="radio" value={thisCompany.timeElapsed} name="moneyStuff" onClick={this.random}/>{thisCompany.timeElapsed} <br/>
+                </>
             }, []);
-        });
+        //});
 
     }
 
@@ -21,14 +29,15 @@ class OneProblemRev extends React.Component {
         return ( 
             <>
                 <div>
+{/*
                      <p name="moneyStuff" onClick={this.random}/>{thisCompany.company}<br/> 
                      <input type="radio" value={thisCompany.ticker} name="moneyStuff"/>{thisCompany.ticker} <br/>
                      <input type="radio" value={thisCompany.stockPrice} name="moneyStuff"/>{thisCompany.stockPrice} <br/>
                      <input type="radio" value={thisCompany.timeElapsed} name="moneyStuff"/>{thisCompany.timeElapsed} <br/>
-                </div>
+*/}  
+                 </div>
             </>
         );
-        
     }
 }
 
