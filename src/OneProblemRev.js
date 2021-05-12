38bb8dc757e556b1c82f92986e7e89a697d047e2
April Day <april.day@gmail.com>
2021-05-12 10:24:07 +0100

    Twentieth commit

--- src/OneProblemRev.js
+++ src/OneProblemRev.js
@@ -4,8 +4,10 @@ import { stockData } from "./data.js";
 
 class OneProblemRev extends React.Component {    
 
-//    let thisCompany = "April";
+    constructor() {
+        super();
 
+    } 
 
     random(){
         //React.useEffect(()=>{
@@ -13,13 +15,16 @@ class OneProblemRev extends React.Component {
                 {/*while (this.state)   NEED TO HAVE A WHILE LOOP STATEMENT HERE*/}
                 const randomElement = Math.floor(Math.random()*stockData.length);
                 console.log("this is the randomElement:", randomElement);
-                const thisCompany = stockData[randomElement];
+                const thisCompany = this.stockData[randomElement];
+
+                {/*}
                 <>
                     <p name="moneyStuff" />{thisCompany.company}<br/> 
                         <input type="radio" value={thisCompany.ticker} name="moneyStuff" onClick={this.random}/>{thisCompany.ticker} <br/>
                         <input type="radio" value={thisCompany.stockPrice} name="moneyStuff" onClick={this.random}/>{thisCompany.stockPrice} <br/>
                         <input type="radio" value={thisCompany.timeElapsed} name="moneyStuff" onClick={this.random}/>{thisCompany.timeElapsed} <br/>
                 </>
+            */}
             }, []);
         //});
 
@@ -28,13 +33,15 @@ class OneProblemRev extends React.Component {
     render (){            
         return ( 
             <>
-                <div>
-{/*
-                     <p name="moneyStuff" onClick={this.random}/>{thisCompany.company}<br/> 
-                     <input type="radio" value={thisCompany.ticker} name="moneyStuff"/>{thisCompany.ticker} <br/>
-                     <input type="radio" value={thisCompany.stockPrice} name="moneyStuff"/>{thisCompany.stockPrice} <br/>
-                     <input type="radio" value={thisCompany.timeElapsed} name="moneyStuff"/>{thisCompany.timeElapsed} <br/>
-*/}  
+                <div onClick= {this.random}>
+
+                     {/*<p name="moneyStuff" onClick={this.random}/>{this.thisCompany.company}<p/>*/}
+                     <p name ="moneyStuff" onClick={this.random}/>{this.thisCompany}<p/>
+                     {/*
+                         <input type="radio" value={this.thisCompany.ticker} name="moneyStuff"/>{this.thisCompany.ticker} <br/>
+                        <input type="radio" value={thisCompany.stockPrice} name="moneyStuff"/>{thisCompany.stockPrice} <br/>
+                        <input type="radio" value={thisCompany.timeElapsed} name="moneyStuff"/>{thisCompany.timeElapsed} <br/>
+                    */}
                  </div>
             </>
         );
