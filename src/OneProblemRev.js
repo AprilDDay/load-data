be8ff1f68c48ff48f124960203808b800c8c3920
April Day <april.day@gmail.com>
2021-05-18 22:34:14 +0100

    Twenty-seventh commit

--- src/OneProblemRev.js
+++ src/OneProblemRev.js
@@ -22,9 +22,9 @@ class OneProblemRev extends React.Component {
            
                 <div >
                      
-                     <p name ="moneyStuff" />{this.thisCompany}<p/>
+                     {/*<p name ="moneyStuff" />{this.thisCompany}<p/>*/}
                      
-                        <input type="radio" value={this.thisCompany.ticker} name="moneyStuff" onClick={this.random()}/>{this.thisCompany.ticker} <br/>
+                        <input type="radio" value={this.thisCompany.ticker} name="moneyStuff" onClick={this.random}/>{this.thisCompany.ticker} <br/>
                         {/*<input type="radio" value={thisCompany.stockPrice} name="moneyStuff" onClick={this.random()}/>{thisCompany.stockPrice} <br/>
                         <input type="radio" value={hisCompany.timeElapsed} name="moneyStuff" onClick={this.random()}/>{thisCompany.timeElapsed} <br/>
         */}
