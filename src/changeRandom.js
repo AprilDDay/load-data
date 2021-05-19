60761d86738f223e7512a09abe39b7c313872caf
April Day <april.day@gmail.com>
2021-05-19 10:15:58 +0100

    Thirty-first commit

--- src/changeRandom.js
+++ src/changeRandom.js
@@ -5,22 +5,34 @@ const Effort = () => {
     const [selected, setSelected] = useState(0);
     const randomNumber = Math.floor(Math.random()*6);
     const correctNumber = 4;
-    if (randomNumber === correctNumber){
-        console.log("that is correct!");
-        //return(
-          //  <div id="correct">That is correct!</div>
-        //)
-
-    }
 
     console.log("selected: ", selected);
     console.log("randomNumber: ", randomNumber);
 
+
+
+    if (randomNumber === correctNumber){
+        console.log("that is correct!");
+//        return(
+  //          <div id="correct">That is correct!</div>
+    //    )
+    } else {
+        console.log("try again!");
+      //  return(
+        //    <div id="incorrect">That is incorrect!</div>
+        //)
+
+    }
+
+    
+
+
 return (
     <div>
-        <button onClick={() => {setSelected(randomNumber)} }>next number</button>
-        <div>{randomNumber}</div>
-    </div> 
+    <button onClick={() => {setSelected(randomNumber)} }>next number</button>
+    <div>{randomNumber}</div>
+</div> 
+
     )
 }
 
