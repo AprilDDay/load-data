a727c14d97e2f0a1be53feec9ca5bc8a69ad4f8b
April Day <april.day@gmail.com>
2021-05-19 09:41:54 +0100

    Thirtieth commit

--- src/changeRandom.js
+++ src/changeRandom.js
@@ -4,6 +4,14 @@ import { useState } from 'react';
 const Effort = () => {
     const [selected, setSelected] = useState(0);
     const randomNumber = Math.floor(Math.random()*6);
+    const correctNumber = 4;
+    if (randomNumber === correctNumber){
+        console.log("that is correct!");
+        //return(
+          //  <div id="correct">That is correct!</div>
+        //)
+
+    }
 
     console.log("selected: ", selected);
     console.log("randomNumber: ", randomNumber);
